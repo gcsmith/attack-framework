82621b5d453dc0403142b467af2334000b1a99c4
Garrett Smith <smith.garrett.c@gmail.com>
2012-08-07 20:43:32 -0400

    misc

--- scripts/collect.py
+++ scripts/collect.py
@@ -16,6 +16,7 @@ class result_collector:
         self.known_diffs = None             # known key differential matrix
         self.guess_confs = None             # estimate confidence matrix
         self.known_confs = None             # known key confidence matrix
+        self.valid_index = []
         self.guess_key   = np.array([])     # estimate key guess
 
     def examine_results(self, exp_key, result_dir, attack_dir):
@@ -59,6 +60,15 @@ class result_collector:
             self.known_confs = data[0]
         self.known_confs = np.vstack([self.known_confs, data[exp_key + 1]])
 
+        valid_idx = -1
+        for i in range(len(data[exp_key + 1])):
+            if data[exp_key + 1][i] > 1.0:
+                valid_idx = i
+                break
+
+        self.valid_index.append(valid_idx * self.report_int)
+        print self.valid_index, ' --> ', max(self.valid_index)
+
     def run(self, num_bytes):
         for i in range(0, num_bytes):
             parm_str = self.parameters + ',byte=' + str(i)
@@ -77,6 +87,10 @@ class result_collector:
 
             self.examine_results(self.known_key[i], self.result_dir, out_path)
 
+        self.valid_index.append(max(self.valid_index))
+
+        np.savetxt(os.path.join(self.result_dir, 'guess_intervals.csv'),
+                   np.array(self.valid_index), fmt='%d', delimiter=',');
         np.savetxt(os.path.join(self.result_dir, 'guess_diffs.csv'),
                    self.guess_diffs.transpose(), delimiter=',');
         np.savetxt(os.path.join(self.result_dir, 'known_diffs.csv'),
@@ -107,45 +121,44 @@ if __name__ == '__main__':
     rijndael_sbox_key = [ 0x00, 0x11, 0x22, 0x33, 0x44, 0x55, 0x66, 0x77, 0x88, 0x99, 0xAA, 0xBB, 0xCC, 0xDD, 0xEE, 0xFF ]
     aes_iterative_key = range(16)
     grostl_serial_key = range(64)
-    
 
-#   experiment('results_rijndael_sbox_dpa_sb',   'trace_rijndael_sbox_50k.bin',   'aes_hd_r0',          'dpa_dp', 20000, 100, 'bits=1,offset=0,thresh=0', 16, 4, rijndael_sbox_key)
-#   experiment('results_rijndael_sbox_dpa_mb',   'trace_rijndael_sbox_50k.bin',   'aes_hd_r0',          'dpa_dp', 20000, 100, 'bits=8,offset=0,thresh=5', 16, 4, rijndael_sbox_key)
-#   experiment('results_rijndael_sbox_cpa_sb',   'trace_rijndael_sbox_50k.bin',   'aes_hd_r0',          'cpa_dp', 20000, 100, 'bits=1,offset=0',          16, 4, rijndael_sbox_key)
-#   experiment('results_rijndael_sbox_cpa_mb',   'trace_rijndael_sbox_50k.bin',   'aes_hd_r0',          'cpa_dp', 20000, 100, 'bits=8,offset=0',          16, 4, rijndael_sbox_key)
+    experiment('results_rijndael_sbox_dpa_sb',   'trace_4k_rijndael_sbox.bin',   'aes_hd_r0',          'dpa_dp', 4000, 5, 'bits=1,offset=0,thresh=0', 16, 5, rijndael_sbox_key)
+    experiment('results_rijndael_sbox_dpa_mb',   'trace_4k_rijndael_sbox.bin',   'aes_hd_r0',          'dpa_dp', 4000, 5, 'bits=8,offset=0,thresh=5', 16, 5, rijndael_sbox_key)
+    experiment('results_rijndael_sbox_cpa_sb',   'trace_4k_rijndael_sbox.bin',   'aes_hd_r0',          'cpa_dp', 4000, 5, 'bits=1,offset=0',          16, 5, rijndael_sbox_key)
+    experiment('results_rijndael_sbox_cpa_mb',   'trace_4k_rijndael_sbox.bin',   'aes_hd_r0',          'cpa_dp', 4000, 5, 'bits=8,offset=0',          16, 5, rijndael_sbox_key)
 
-#   experiment('results_rijndael_sbox_m_dpa_sb', 'trace_rijndael_sbox_m_50k.bin', 'aes_hd_r0',          'dpa_dp', 50000, 100, 'bits=1,offset=0,thresh=0', 16, 4, rijndael_sbox_key)
-#   experiment('results_rijndael_sbox_m_dpa_mb', 'trace_rijndael_sbox_m_50k.bin', 'aes_hd_r0',          'dpa_dp', 50000, 100, 'bits=8,offset=0,thresh=5', 16, 4, rijndael_sbox_key)
-#   experiment('results_rijndael_sbox_m_cpa_sb', 'trace_rijndael_sbox_m_50k.bin', 'aes_hd_r0',          'cpa_dp', 50000, 100, 'bits=1,offset=0',          16, 4, rijndael_sbox_key)
-#   experiment('results_rijndael_sbox_m_cpa_mb', 'trace_rijndael_sbox_m_50k.bin', 'aes_hd_r0',          'cpa_dp', 50000, 100, 'bits=8,offset=0',          16, 4, rijndael_sbox_key)
+#   experiment('results_rijndael_sbox_m_dpa_sb', 'trace_4k_rijndael_sbox_m.bin', 'aes_hd_r0',          'dpa_dp', 50000, 100, 'bits=1,offset=0,thresh=0', 16, 4, rijndael_sbox_key)
+#   experiment('results_rijndael_sbox_m_dpa_mb', 'trace_4k_rijndael_sbox_m.bin', 'aes_hd_r0',          'dpa_dp', 50000, 100, 'bits=8,offset=0,thresh=5', 16, 4, rijndael_sbox_key)
+#   experiment('results_rijndael_sbox_m_cpa_sb', 'trace_4k_rijndael_sbox_m.bin', 'aes_hd_r0',          'cpa_dp', 50000, 100, 'bits=1,offset=0',          16, 4, rijndael_sbox_key)
+#   experiment('results_rijndael_sbox_m_cpa_mb', 'trace_4k_rijndael_sbox_m.bin', 'aes_hd_r0',          'cpa_dp', 50000, 100, 'bits=8,offset=0',          16, 4, rijndael_sbox_key)
 
-#   experiment('results_aes_iterative_dpa_sb',   'trace_aes_iterative_50k.bin',   'aes_hw_r0',          'dpa_dp', 20000, 100, 'bits=1,offset=0,thresh=0', 16, 4, aes_iterative_key)
-#   experiment('results_aes_iterative_dpa_mb',   'trace_aes_iterative_50k.bin',   'aes_hw_r0',          'dpa_dp', 20000, 100, 'bits=8,offset=0,thresh=5', 16, 4, aes_iterative_key)
-#   experiment('results_aes_iterative_cpa_sb',   'trace_aes_iterative_50k.bin',   'aes_hw_r0',          'cpa_dp', 20000, 100, 'bits=1,offset=0',          16, 4, aes_iterative_key)
-#   experiment('results_aes_iterative_cpa_mb',   'trace_aes_iterative_50k.bin',   'aes_hw_r0',          'cpa_dp', 20000, 100, 'bits=8,offset=0',          16, 4, aes_iterative_key)
+    experiment('results_aes_iterative_dpa_sb',   'trace_4k_aes_iterative.bin',   'aes_hw_r0',          'dpa_dp', 4000, 5, 'bits=1,offset=0,thresh=0', 16, 5, aes_iterative_key)
+    experiment('results_aes_iterative_dpa_mb',   'trace_4k_aes_iterative.bin',   'aes_hw_r0',          'dpa_dp', 4000, 5, 'bits=8,offset=0,thresh=5', 16, 5, aes_iterative_key)
+    experiment('results_aes_iterative_cpa_sb',   'trace_4k_aes_iterative.bin',   'aes_hw_r0',          'cpa_dp', 4000, 5, 'bits=1,offset=0',          16, 5, aes_iterative_key)
+    experiment('results_aes_iterative_cpa_mb',   'trace_4k_aes_iterative.bin',   'aes_hw_r0',          'cpa_dp', 4000, 5, 'bits=8,offset=0',          16, 5, aes_iterative_key)
 
-#   experiment('results_aes_iterative_m_dpa_sb', 'trace_aes_iterative_m_50k.bin', 'aes_hw_r0',          'dpa_dp', 50000, 100, 'bits=1,offset=0,thresh=0', 16, 4, aes_iterative_key)
-#   experiment('results_aes_iterative_m_dpa_mb', 'trace_aes_iterative_m_50k.bin', 'aes_hw_r0',          'dpa_dp', 50000, 100, 'bits=8,offset=0,thresh=5', 16, 4, aes_iterative_key)
-#   experiment('results_aes_iterative_m_cpa_sb', 'trace_aes_iterative_m_50k.bin', 'aes_hw_r0',          'cpa_dp', 50000, 100, 'bits=1,offset=0',          16, 4, aes_iterative_key)
-#   experiment('results_aes_iterative_m_cpa_mb', 'trace_aes_iterative_m_50k.bin', 'aes_hw_r0',          'cpa_dp', 50000, 100, 'bits=8,offset=0',          16, 4, aes_iterative_key)
+#   experiment('results_aes_iterative_m_dpa_sb', 'trace_4k_aes_iterative_m.bin', 'aes_hw_r0',          'dpa_dp', 50000, 100, 'bits=1,offset=0,thresh=0', 16, 4, aes_iterative_key)
+#   experiment('results_aes_iterative_m_dpa_mb', 'trace_4k_aes_iterative_m.bin', 'aes_hw_r0',          'dpa_dp', 50000, 100, 'bits=8,offset=0,thresh=5', 16, 4, aes_iterative_key)
+#   experiment('results_aes_iterative_m_cpa_sb', 'trace_4k_aes_iterative_m.bin', 'aes_hw_r0',          'cpa_dp', 50000, 100, 'bits=1,offset=0',          16, 4, aes_iterative_key)
+#   experiment('results_aes_iterative_m_cpa_mb', 'trace_4k_aes_iterative_m.bin', 'aes_hw_r0',          'cpa_dp', 50000, 100, 'bits=8,offset=0',          16, 4, aes_iterative_key)
 
-#   experiment('results_grostl_dp512_dpa_sb',    'trace_grostl_dp512_50k.bin',    'grostl_dp512_hd_r0', 'dpa_dp', 20000, 100, 'bits=1,offset=0,thresh=0', 64, 4, grostl_serial_key)
-#   experiment('results_grostl_dp512_dpa_mb',    'trace_grostl_dp512_50k.bin',    'grostl_dp512_hd_r0', 'dpa_dp', 50000, 100, 'bits=8,offset=0,thresh=5', 64, 4, grostl_serial_key)
-#   experiment('results_grostl_dp512_cpa_sb',    'trace_grostl_dp512_50k.bin',    'grostl_dp512_hd_r0', 'cpa_dp', 50000, 100, 'bits=1,offset=0',          64, 4, grostl_serial_key)
-#   experiment('results_grostl_dp512_cpa_mb',    'trace_grostl_dp512_50k.bin',    'grostl_dp512_hd_r0', 'cpa_dp', 20000, 100, 'bits=8,offset=0',          64, 4, grostl_serial_key)
+    experiment('results_grostl_dp512_dpa_sb',    'trace_4k_grostl_dp512.bin',    'grostl_dp512_hd_r0', 'dpa_dp', 4000, 5, 'bits=1,offset=0,thresh=0', 64, 5, grostl_serial_key)
+    experiment('results_grostl_dp512_dpa_mb',    'trace_4k_grostl_dp512.bin',    'grostl_dp512_hd_r0', 'dpa_dp', 4000, 5, 'bits=8,offset=0,thresh=5', 64, 5, grostl_serial_key)
+    experiment('results_grostl_dp512_cpa_sb',    'trace_4k_grostl_dp512.bin',    'grostl_dp512_hd_r0', 'cpa_dp', 4000, 5, 'bits=1,offset=0',          64, 5, grostl_serial_key)
+    experiment('results_grostl_dp512_cpa_mb',    'trace_4k_grostl_dp512.bin',    'grostl_dp512_hd_r0', 'cpa_dp', 4000, 5, 'bits=8,offset=0',          64, 5, grostl_serial_key)
 
-#   experiment('results_grostl_dp512_m_dpa_sb',  'trace_grostl_dp512_m_50k.bin',  'grostl_dp512_hd_r0', 'dpa_dp', 50000, 100, 'bits=1,offset=0,thresh=0', 64, 4, grostl_serial_key)
-#   experiment('results_grostl_dp512_m_dpa_mb',  'trace_grostl_dp512_m_50k.bin',  'grostl_dp512_hd_r0', 'dpa_dp', 50000, 100, 'bits=8,offset=0,thresh=5', 64, 4, grostl_serial_key)
-#   experiment('results_grostl_dp512_m_cpa_sb',  'trace_grostl_dp512_m_50k.bin',  'grostl_dp512_hd_r0', 'cpa_dp', 50000, 100, 'bits=1,offset=0',          64, 4, grostl_serial_key)
-#   experiment('results_grostl_dp512_m_cpa_mb',  'trace_grostl_dp512_m_50k.bin',  'grostl_dp512_hd_r0', 'cpa_dp', 50000, 100, 'bits=8,offset=0',          64, 4, grostl_serial_key)
+#   experiment('results_grostl_dp512_m_dpa_sb',  'trace_4k_grostl_dp512_m.bin',  'grostl_dp512_hd_r0', 'dpa_dp', 50000, 100, 'bits=1,offset=0,thresh=0', 64, 4, grostl_serial_key)
+#   experiment('results_grostl_dp512_m_dpa_mb',  'trace_4k_grostl_dp512_m.bin',  'grostl_dp512_hd_r0', 'dpa_dp', 50000, 100, 'bits=8,offset=0,thresh=5', 64, 4, grostl_serial_key)
+#   experiment('results_grostl_dp512_m_cpa_sb',  'trace_4k_grostl_dp512_m.bin',  'grostl_dp512_hd_r0', 'cpa_dp', 50000, 100, 'bits=1,offset=0',          64, 4, grostl_serial_key)
+#   experiment('results_grostl_dp512_m_cpa_mb',  'trace_4k_grostl_dp512_m.bin',  'grostl_dp512_hd_r0', 'cpa_dp', 50000, 100, 'bits=8,offset=0',          64, 4, grostl_serial_key)
 
-#   experiment('results_grostl_dp64_dpa_sb',     'trace_grostl_dp64_50k.bin',     'grostl_dp64_hd_r0',  'dpa_dp', 20000, 100, 'bits=1,offset=0,thresh=5', 64, 4, grostl_serial_key)
-#   experiment('results_grostl_dp64_dpa_mb',     'trace_grostl_dp64_50k.bin',     'grostl_dp64_hd_r0',  'dpa_dp', 50000, 100, 'bits=8,offset=0,thresh=5', 64, 4, grostl_serial_key)
-#   experiment('results_grostl_dp64_cpa_sb',     'trace_grostl_dp64_50k.bin',     'grostl_dp64_hd_r0',  'cpa_dp', 20000, 100, 'bits=1,offset=0',          64, 4, grostl_serial_key)
-#   experiment('results_grostl_dp64_cpa_mb',     'trace_grostl_dp64_50k.bin',     'grostl_dp64_hd_r0',  'cpa_dp', 20000, 100, 'bits=8,offset=0',          64, 4, grostl_serial_key)
+    experiment('results_grostl_dp64_dpa_sb',     'trace_4k_grostl_dp64.bin',     'grostl_dp64_hd_r0',  'dpa_dp', 4000, 5, 'bits=1,offset=0,thresh=5', 64, 5, grostl_serial_key)
+    experiment('results_grostl_dp64_dpa_mb',     'trace_4k_grostl_dp64.bin',     'grostl_dp64_hd_r0',  'dpa_dp', 4000, 5, 'bits=8,offset=0,thresh=5', 64, 5, grostl_serial_key)
+    experiment('results_grostl_dp64_cpa_sb',     'trace_4k_grostl_dp64.bin',     'grostl_dp64_hd_r0',  'cpa_dp', 4000, 5, 'bits=1,offset=0',          64, 5, grostl_serial_key)
+    experiment('results_grostl_dp64_cpa_mb',     'trace_4k_grostl_dp64.bin',     'grostl_dp64_hd_r0',  'cpa_dp', 4000, 5, 'bits=8,offset=0',          64, 5, grostl_serial_key)
 
-#   experiment('results_grostl_dp64_m_dpa_sb',   'trace_grostl_dp64_m_50k.bin',   'grostl_dp64_hd_r0',  'dpa_dp', 50000, 100, 'bits=1,offset=0,thresh=0', 64, 4, grostl_serial_key)
-#   experiment('results_grostl_dp64_m_dpa_mb',   'trace_grostl_dp64_m_50k.bin',   'grostl_dp64_hd_r0',  'dpa_dp', 50000, 100, 'bits=8,offset=0,thresh=5', 64, 4, grostl_serial_key)
-    experiment('results_grostl_dp64_m_cpa_sb',   'trace_grostl_dp64_m_50k.bin',   'grostl_dp64_hd_r0',  'cpa_dp', 50000, 100, 'bits=1,offset=0',          64, 4, grostl_serial_key)
-    experiment('results_grostl_dp64_m_cpa_mb',   'trace_grostl_dp64_m_50k.bin',   'grostl_dp64_hd_r0',  'cpa_dp', 50000, 100, 'bits=8,offset=0',          64, 4, grostl_serial_key)
+#   experiment('results_grostl_dp64_m_dpa_sb',   'trace_4k_grostl_dp64_m.bin',   'grostl_dp64_hd_r0',  'dpa_dp', 50000, 100, 'bits=1,offset=0,thresh=0', 64, 4, grostl_serial_key)
+#   experiment('results_grostl_dp64_m_dpa_mb',   'trace_4k_grostl_dp64_m.bin',   'grostl_dp64_hd_r0',  'dpa_dp', 50000, 100, 'bits=8,offset=0,thresh=5', 64, 4, grostl_serial_key)
+#   experiment('results_grostl_dp64_m_cpa_sb',   'trace_4k_grostl_dp64_m.bin',   'grostl_dp64_hd_r0',  'cpa_dp', 50000, 100, 'bits=1,offset=0',          64, 4, grostl_serial_key)
+#   experiment('results_grostl_dp64_m_cpa_mb',   'trace_4k_grostl_dp64_m.bin',   'grostl_dp64_hd_r0',  'cpa_dp', 50000, 100, 'bits=8,offset=0',          64, 4, grostl_serial_key)
 
